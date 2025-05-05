53c7f64e851d814eb97a4279edd8a1d9c15d4cd7
maxwong3 <maxwon7@gmail.com>
2025-05-04 22:42:51 -0400

    Next: implement Selenium for dynamic tables

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def main():
 def hello(name):
     return f"Hello, {escape(name)}!"
 
-url = 'https://baseballsavant.mlb.com/leaderboard/custom?year=2025&type=batter&filter=&min=10&selections=pa%2Ck_percent%2Cbb_percent%2Cwoba%2Cxwoba%2Csweet_spot_percent%2Cbarrel_batted_rate%2Chard_hit_percent%2Cavg_best_speed%2Cavg_hyper_speed%2Cwhiff_percent%2Cswing_percent&chart=false&x=pa&y=pa&r=no&chartType=beeswarm&sort=xwoba&sortDir=desc'
+url = 'https://www.baseball-reference.com/leagues/majors/2025-standard-batting.shtml'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-table = pd.read_html(url)
+table = soup.find_all('table')
 
 print(table)
